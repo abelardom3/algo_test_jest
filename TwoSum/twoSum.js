2b32079ed61d06c7fa078848bf7331d77de64110
abelardom3 <abelardom1999@gmail.com>
2022-12-21 17:05:46 -0500

    added new algo

--- TwoSum/twoSum.js
+++ TwoSum/twoSum.js
@@ -2,20 +2,42 @@
 const array = [1, 2, 4, 8]
 const sum = 12
 
-const validInput = (array, sum) => {
-    const past = []
-    for (let i = 0; i < array.length; i++) {
-        let current = array[i]
+// const validInput = (array, sum) => {
+//     const past = []
+//     for (let i = 0; i < array.length; i++) {
+//         let current = array[i]
+//         let neededNum = sum - current
+//         if (!past.includes(neededNum)) {
+//             past.push(current)
+//         } else {
+//             return true
+//         }
+//     }
+
+//     return false
+
+// }
+
+
+// Two sum that returns an array with the indexes that add up to the sum 
+
+const validInput = (arr, sum) => {
+    const cont = []
+    for (let i = 0; i < arr.length; i++) {
+        let current = arr[i]
         let neededNum = sum - current
-        if (!past.includes(neededNum)) {
-            past.push(current)
+        if (!cont.includes(neededNum)) {
+            cont.push(current)
         } else {
-            return true
+            return [cont.indexOf(neededNum), i]
         }
     }
-
-    return false
-
+    return -1
 }
 
-module.exports = validInput
+
+console.log(validInput(array, sum))
+
+
+
+//module.exports = validInput
---
+++ testFolder/algo.js
@@ -0,0 +1,14 @@
+
+
+// testing 
+
+var number = 20
+
+
+const display = function () {
+    console.log(number)
+}
+
+
+
+display()
