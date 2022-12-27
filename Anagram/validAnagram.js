2fcf7b5992abdd094282e0bdaf066bce99a5da41
abelardom3 <abelardom1999@gmail.com>
2022-12-27 13:05:09 -0500

    added algo

---
+++ Anagram/validAnagram.js
@@ -0,0 +1,40 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Input: s = "rat", t = "car"
+Output: false
+
+*/
+
+
+const s = 'anagram'
+const t = 'nagaram'
+
+const isAnagram = (s, t) => {
+    const sletter = s.split('')
+    const tletter = t.split('')
+    const obj = {}
+
+    if (sletter.length !== tletter.length) return false
+
+    sletter.forEach(letter => {
+        !obj[letter] ? obj[letter] = 1 : obj[letter]++
+    })
+
+    let result = true
+
+    tletter.forEach(letter => {
+        if (obj[letter]) {
+            obj[letter]--
+            if (obj[letter] < 0) result = false
+        } else {
+            result = false
+        }
+    })
+    return result
+}
+
+console.log(isAnagram(s, t))
