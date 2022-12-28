30a238bcaf23a09a3626abe0bda0d3ac61a797c4
abelardom3 <abelardom1999@gmail.com>
2022-12-28 15:53:57 -0500

    adding new algo

---
+++ Frequent/KFrequent.js
@@ -0,0 +1,20 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+Input: nums = [1], k = 1
+Output: [1]
+
+*/
+const nums = [1, 1, 1, 2, 2, 3]
+const k = 2
+
+
+const kFrequent = (nums, k) => {
+
+
+}
+
+console.log(kFrequent(nums, k))
