d60710663e83278ff5123f63c76287cf3dbc486e
abelardom3 <abelardom1999@gmail.com>
2023-01-02 14:07:33 -0500

    added k freq algo

--- Frequent/KFrequent.js
+++ Frequent/KFrequent.js
@@ -13,8 +13,31 @@ const k = 2
 
 
 const kFrequent = (nums, k) => {
+    const map = {}
 
+    for (let num of nums) {
+        !map[num] ? map[num] = 1 : map[num]++
+    }
 
+    let bucket = []
+    for (let i = 0; i <= nums.length; i++) {
+        bucket.push([])
+    }
+
+    for (let key in map) {
+        let count = map[key]
+        bucket[count].push(key)
+    }
+    let result = []
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (bucket[i].length === 0) {
+            continue
+        } else {
+            result = [...result, ...bucket[i]]
+        }
+
+    }
+    return result.slice(0, k)
 }
 
 console.log(kFrequent(nums, k))
